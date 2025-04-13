6e52c8613643958d648862347f803df7198e25ff
niklasovaska <niklas.ovaska@gmail.com>
2025-04-13 19:38:30 +0300

    handleLogin to get jwtToken

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import axios from 'axios'
+import Albumlist from "./Albumlist"
 
 
 type User = {
@@ -12,10 +13,49 @@ const Login = () => {
     const [user, setUser] = useState<User>({username: '', password: ''})
     const [isAuthenticated, setAuth] = useState(false)
 
-    return(
-        <>
-        </>
-    )
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        {
+            setUser({...user,
+                [event.target.name] : event.target.value
+            })
+        }
+    }
+
+    const handleLogin = () => {
+        axios.post('http://localhost:8080/login', user, {
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(res => {
+            const jwtToken = res.headers.authorization
+
+            if(jwtToken !== null) {
+                sessionStorage.setItem('jwt', jwtToken)
+                setAuth(true)
+            }
+
+        })
+        .catch(error => console.log(error))
+    }
+
+    if(isAuthenticated) {
+        return <Albumlist />
+    }
+    else {
+        return(
+            <div style={{display: 'flex', flexDirection: 'column'}}>
+                <div style={{display: 'flex', gap: 4}}>
+                    <label htmlFor='username'></label>
+                    <input name='username' id='username' onChange={handleChange}/>
+                </div>
+                <div style={{display: 'flex', gap: 4}}>
+                    <label htmlFor='password'></label>
+                    <input name='password' id='password' onChange={handleChange}/>
+                </div>
+                <button onClick={handleLogin}>Login</button>
+                
+            </div>
+        )  
+    }
 }
 
 export default Login
