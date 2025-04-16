58ab2f2dfef22f50e3d678bd1d15c4dd654a0b74
niklasovaska <niklas.ovaska@gmail.com>
2025-04-16 16:52:41 +0300

    deleteAlbum, getArtists, addArtist

--- src/services/albumService.ts
+++ src/services/albumService.ts
@@ -1,4 +1,4 @@
-import { AlbumResponse } from '../types'
+import { AlbumResponse, Artist, ArtistResponse } from '../types'
 import axios, { AxiosRequestConfig } from 'axios'
 
 const baseUrl = 'http://localhost:8080/api'
@@ -20,6 +20,34 @@ export const getAlbums = async (): Promise<AlbumResponse[]> => {
     return response.data
 }
 
-// delete
-// add
-// update
+export const deleteAlbum = async (id: number): Promise<AlbumResponse> => {
+    const response = await axios.delete(`${baseUrl}/albums/${id}`, getAxiosConfig())
+
+    return response.data
+}
+
+// const addAlbum = async (album: Album): Promise<AlbumResponse> => {
+//     const response = await axios.post(`${baseUrl}/albums`)
+
+//     return response.data
+// }
+
+// const updateAlbum = async (albumEntry: AlbumEntry): Promise<AlbumResponse> => {
+//     const response = await axios.put(albumEntry.url, albumEntry.album, getAxiosConfig())
+// }
+
+export const getArtists = async (): Promise<ArtistResponse> => {
+    const response = await axios.get(`${baseUrl}/artists`, getAxiosConfig())
+
+    return response.data._embedded.artists
+}
+
+export const addArtist = async (artist: Artist): Promise<ArtistResponse> => {
+    const response = await axios.post(`${baseUrl}/artists`, artist, getAxiosConfig())
+        
+    return response.data
+}
+
+
+
+
