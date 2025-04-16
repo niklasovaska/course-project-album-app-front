ccb65f10b905e035e2c78610d890f0ea7e3fb354
niklasovaska <niklas.ovaska@gmail.com>
2025-04-16 16:55:10 +0300

    useMutation with deleteAlbum as mutationFn

--- src/components/Albumlist.tsx
+++ src/components/Albumlist.tsx
@@ -1,12 +1,26 @@
-import { getAlbums } from '../services/albumService'
+import { getAlbums, deleteAlbum } from '../services/albumService'
 import { AlbumResponse } from '../types'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import AddArtist from './AddArtist'
 
 const Albumlist = () => {
   
+    const queryClient = useQueryClient()
+
     const { data, error, isSuccess } = useQuery({
         queryKey: ['albums'],
         queryFn: getAlbums
+    })
+
+    const { mutate } = useMutation({
+        mutationFn: deleteAlbum,
+        onError: (err) => {
+            console.log(err)
+        },
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ['albums']})
+            console.log('Successfully deleted: ', data)
+        }
     })
 
     if(!isSuccess) {
@@ -17,18 +31,23 @@ const Albumlist = () => {
     }
     else {
         return(
-            <table>
-                <tbody>
-                    {data.map((album: AlbumResponse) => 
-                    <tr key={album.id}>
-                        <td>{album.title}</td>
-                        <td>{album.artist.name}</td>
-                        <td>{album.releaseYear}</td>
-                        <td>{album.rating}</td>
-                    </tr>)
-                    }
-                </tbody>
-            </table>
+            <>
+                <table>
+                    <tbody>
+                        {data.map((album: AlbumResponse) => 
+                        <tr key={album.id}>
+                            <td>{album.title}</td>
+                            <td>{album.artist.name}</td>
+                            <td>{album.releaseYear}</td>
+                            <td>{album.rating}</td>
+                            <td><button onClick={() => mutate(album.id)}>Delete</button></td>
+                        </tr>)
+                        }
+                    </tbody>
+                </table>
+                <AddArtist />
+            </>
+            
         )
     }
 }
