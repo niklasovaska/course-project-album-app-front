1549de7c56a5306f453ed8d2d59b24df490f85d6
niklasovaska <niklas.ovaska@gmail.com>
2025-04-27 10:15:50 +0300

    themeprovider and toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from './components/theme-provider'
 import Login from './components/Login'
+import { Toaster } from "@/components/ui/sonner"
 
 const queryClient = new QueryClient()
 
 function App() {
   
     return(
-      <>
-        <QueryClientProvider client={queryClient}>
-          <Login />
-        </QueryClientProvider>
-      </>
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <div className='flex items-center justify-center h-screen'>
+          <Toaster richColors/>
+          <QueryClientProvider client={queryClient}>
+            <Login />
+          </QueryClientProvider>
+        </div>
+      </ThemeProvider>
     )
   
 }
