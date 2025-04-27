b2912040b22b3a0286fd9ea3ad3dd7149ce4fb7d
niklasovaska <niklas.ovaska@gmail.com>
2025-04-27 10:13:45 +0300

    login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react"
 import axios from 'axios'
 import { Button } from "../components/ui/button"
+import { 
+        Card, 
+        CardContent,
+        CardDescription,
+        CardFooter,
+        CardHeader,
+        CardTitle, 
+} from "../components/ui/card"
+import { Input } from "../components/ui/input"
+import { Label } from "../components/ui/label"
 import Albumlist from "./Albumlist"
+import { toast } from "sonner"
 
 
 type User = {
@@ -13,9 +24,11 @@ const Login = () => {
 
     const [user, setUser] = useState<User>({username: '', password: ''})
     const [isAuthenticated, setAuth] = useState(false)
+    const [formError, setFormError] = useState(false)
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         {
+            setFormError(false)
             setUser({...user,
                 [event.target.name] : event.target.value
             })
@@ -35,26 +48,41 @@ const Login = () => {
             }
 
         })
-        .catch(error => console.log(error))
+        .catch(() => setFormError(true))
+    }
+
+    const handleLogout = () => {
+        setAuth(false)
+        sessionStorage.setItem("jwt", "")
+        toast.info('Logged out successfully')
+        
     }
 
     if(isAuthenticated) {
-        return <Albumlist />
+        return <Albumlist logOut={handleLogout}/>
     }
     else {
         return(
-            <div style={{display: 'flex', flexDirection: 'column', gap: 4}}>
-                <div style={{display: 'flex', gap: 4}}>
-                    <label htmlFor='username'>Username: </label>
-                    <input name='username' id='username' onChange={handleChange}/>
-                </div>
-                <div style={{display: 'flex', gap: 4}}>
-                    <label htmlFor='password'>Password: </label>
-                    <input name='password' id='password' onChange={handleChange}/>
-                </div>
-                <Button onClick={handleLogin}>Login</Button>
-                
-            </div>
+            <Card className='w-full max-w-sm'>
+                <CardHeader>
+                    <CardTitle className='text-2xl'>Login</CardTitle>
+                    <CardDescription>Enter your credentials to login</CardDescription>
+                </CardHeader>
+                <CardContent className='grid gap-4'>
+                    <div className='grid gap-2'>
+                        <Label htmlFor='username'>Username</Label>
+                        <Input name='username' id='username' onChange={handleChange} required />
+                    </div>
+                    <div className='grid gap-2'>
+                        <Label htmlFor='password'>Password</Label>
+                        <Input name='password' id='password' type='password' onChange={handleChange} required />
+                    </div>
+                    {formError && <CardDescription className='text-red-500'>Incorrect username or password</CardDescription>}
+                </CardContent>
+                <CardFooter>
+                    <Button className='w-full' onClick={handleLogin}>Login</Button>
+                </CardFooter>
+            </Card>
         )  
     }
 }
